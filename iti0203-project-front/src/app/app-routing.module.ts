779f65f304e39e4525ba35176618cb5075f48310
jjaaks <jjaaks@ttu.ee>
2020-09-28 20:06:21 +0300

    #5 added, added navigation functionalities

--- iti0203-project-front/src/app/app-routing.module.ts
+++ iti0203-project-front/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import {HomeComponent} from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
+import {RoomDetailComponent} from "./room-detail/room-detail.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'rooms', component: RoomsComponent },
+  { path: 'detail/:id', component: RoomDetailComponent },
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent }
 ];
