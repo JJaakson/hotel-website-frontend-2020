bbbde6b1d957f96f574fd08a4ade2d281ee746bd
rjuur <rajuur@ttu.ee>
2020-10-07 18:18:12 +0300

    fixed line (room.service line 6) so no error occurs

--- iti0203-project-front/src/app/room.service.ts
+++ iti0203-project-front/src/app/room.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import { ROOMS } from './mock-rooms';
 
 import { Room } from './room';
 import { MessageService } from './message.service';
