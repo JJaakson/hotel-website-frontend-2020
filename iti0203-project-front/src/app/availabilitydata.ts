6e709dc675379675af94ac4a0152c6babff0546f
Janar Keit Jaakson <jjaaks@ttu.ee>
2020-10-13 15:11:05 +0300

    #14 Remaking of bookings, booking contains room object

--- iti0203-project-front/src/app/availabilitydata.ts
+++ iti0203-project-front/src/app/availabilitydata.ts
@@ -1,7 +1,5 @@
-import {Room} from "./room";
-
 export interface Availabilitydata {
-  room: Room;
+  roomId: number;
   startDate: string;
   endDate: string;
 }
--- iti0203-project-front/src/app/booking/booking.component.ts
+++ iti0203-project-front/src/app/booking/booking.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit} from '@angular/core';
 import {BookingService} from "../booking.service";
 import {Booking} from "../booking";
-import {Availabilitydata} from "../availabilitydata";
 import {Room} from "../room";
 import {MessageService} from "../message.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {RoomService} from "../room.service";
+import {Availabilitydata} from "../availabilitydata";
 
 @Component({
   selector: 'app-booking',
@@ -14,10 +14,11 @@ import {RoomService} from "../room.service";
 })
 export class BookingComponent implements OnInit {
 
-  bookings: Booking[];
   selectedRoom: Room;
   rooms: Room[];
+  bookings: Booking[];
   roomsForm: FormGroup;
+  availabilityData: Availabilitydata;
 
   constructor(private bookingService: BookingService, private roomService: RoomService,
               private messageService: MessageService, private fb: FormBuilder) { }
@@ -25,28 +26,31 @@ export class BookingComponent implements OnInit {
   ngOnInit() {
     this.getRooms()
     this.roomsForm = this.fb.group({
-      room: []
+      roomControl: []
     })
   }
 
   sendAvailabilityData(roomIdAsString: String, startDate: String, endDate: String): void {
-    roomIdAsString = roomIdAsString.trim();
-    let roomId = Number(roomIdAsString);
-  }
-
-  addBooking(name: String, startDate: String, endDate: String, paymentInfo: String, roomIdAsString: String): void {
-    name = name.trim();
-    paymentInfo = paymentInfo.trim();
     startDate = startDate.trim();
     endDate = endDate.trim();
     roomIdAsString = roomIdAsString.trim();
     let roomId = Number(roomIdAsString);
-    if (!name || !startDate || !endDate || !roomId) { return; }
-    this.bookingService.addBooking({ name, startDate, endDate, roomId, paymentInfo } as Booking)
+    if (!startDate || !endDate || !roomId) { return; }
+    this.bookingService.getAvailabilityData({roomId , startDate, endDate } as Availabilitydata)
+      .subscribe(room => this.selectedRoom = room);
+    this.messageService.add(`BookingsComponent: Got availability data ${this.selectedRoom.id}`);
+  }
+
+  addBooking(name: String, startDate: String, endDate: String, paymentInfo: String, room: Room): void {
+    name = name.trim();
+    paymentInfo = paymentInfo.trim();
+    startDate = startDate.trim();
+    endDate = endDate.trim();
+    if (!name || !startDate || !endDate || !room) { return; }
+    this.bookingService.addBooking({ name, startDate, endDate, room, paymentInfo } as Booking)
       .subscribe(booking => {
         this.bookings.push(booking)
       })
-    this.messageService.add(`RoomDetailComponent: Selected for booking=${name}`);
   }
 
   isItReadyForBooking(): boolean {
